9fb7d78e29f628830d61600e05aef5fe5d82b5a3
Steve Park <deugsun.park@gmail.com>
2021-09-20 23:48:49 +0900

    Add NewsScrapper Initial Version
    request, beautifulsoup 및 ayncio 를 통한 처리
    파일 및 DB 저장 기능 없이 스크래핑 기능만 구현

--- README.md
+++ README.md
@@ -1,11 +1,43 @@
 # codingchallenge-work
-## 대상 사이트 선정 기준
-1. 사이트의 Hit 수가 도움이 될 것
-2. 사이트에서 Scrapping 을 허용할 것
-3. 콘텐츠의 저작권 등에 문제가 없을 것
-- udemy.com : 키워드 기반의 강좌 정보 조회 / LectureScrapper
-- 포탈 사이트 : 키워드 기반의 이미지 검색 / ImageScrapper
-## 코드 작성 시 유의 사항
-1. request, urllib 활용 / LectureScrapper
-2. selenium 의 사용 / ImageScrapper
-3. 이전 강의와 연속된 형태 (python 기초, html_css, JS 등)
+## 사이트 선정 기준
+* 사이트의 Hit 수가 도움이 될 것
+* 사이트에서 Scrapping 을 허용할 것 (robots.txt 파일 확인)
+* 콘텐츠의 저작권 등에 문제가 없을 것
+1. 네이버 뉴스 : 키워드 기반의 뉴스 기사 조회 > NewsScrapper
+2. 포탈 사이트 : 키워드 기반의 이미지 검색 > ImageScrapper
+
+
+## NewsScrapper
+### 1. 사이트 분석
+* 로그인 여부와 관계 없이 동작
+* JavaScript 등을 통한 HTML 페이지 표시 후 rendering 없음
+* 여러 페이지를 동시 요청 가능
+* URL 구조
+> https://search.naver.com/search.naver?where=news&query={검색어}&start={(page_no -1)*10+1}
+* 화면 예시
+![검색화면 예시](./images/naver-news-sample.png)
+### 2. class 구성
+* requests, urllib.parse, beautifulsoup 등 사용
+  * requests: 웹 페이지 요청
+  * urllib.parse: quote 를 통한 한글 검색어 처리
+  * beautifulsoup: 웹 페이지 parsing, CSS Selector 사용
+* `__init__` : 검색어 및 최대 스크래핑 페이지 수 (default: 20)
+* 각 함수의 로그성 메시지는 화면에 표시하고 호출 단계에 따라 tab 으로 들여쓰기
+* 클래스 사용 시 gather 함수를 통하여 ayncio 에 대한 사용자 고려 없이 호출하도록 구성
+* 비동기 호출 구조 : gather() -> __scrap_main() -> __scrap_page()
+    * `gather()` : 호출 사용자 편리성 제공
+    * `__scrap_main()` : 기본 10개의 scrap_page 르 비동기 호출
+    * `__scrap_page()` : 1~10초 중 임의 대기 호 1개의 패이지 스크래핑
+```
+def gather():
+  await asyncio.run(self.__scrap_main())
+
+async def __scrap_main():
+  for page in range(start + 1, end + 1):
+    scrapper.append(asyncio.create_task(self.__scrap_page(page,header)))
+ 
+  await asyncio.gather(*scrapper)
+
+async def __scrap_page():
+  await asyncio.sleep(1000)
+```
---
+++ works/scrapper.py
@@ -0,0 +1,125 @@
+import requests
+from urllib import parse
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import asyncio
+import random
+import itertools
+import time
+
+
+class NewsScrapper:
+    def __init__(self, keyword, maxpages=20):
+        """네이버 키워드 뉴스 스크래퍼 생성자
+
+        Args:
+            keyword (str): 검색어
+            maxpages (int, optional): 최대 검색 페이지. Defaults to 10.
+        """
+        self.baseurl = f'https://search.naver.com/search.naver?where=news&query={parse.quote(keyword)}'
+        self.keyword = keyword
+        self.current = 0
+        self.maximum = maxpages if maxpages < 20 else 20
+
+        random.seed(777)
+
+    async def __scrap_page(self, page, header):
+        """개별 페이지 스크래핑 비동기 처리 함수
+
+        Args:
+            page (int): 대상 페이지 No
+            header (dict): request header
+
+        Raises:
+            Exception: response code가 200 이 아닌 경우 발생
+
+        Returns:
+            dict: 해당 페이지의 기사 장보를 dictionary 형태로 반환 - 최대 10개
+        """
+        baseurl = f'{self.baseurl}&start={(page - 1) * 10 + 1}'
+
+        await asyncio.sleep(random.randint(1, 10))
+
+        response = requests.get(self.baseurl, headers=header)
+        print(f'\t\t[scrap_page] {page} - {response.status_code} / {baseurl}')
+
+        if not response.ok:
+            raise Exception(response.status_code)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        page_result = []
+
+        for item in soup.select('div.news_area'):
+            article = {}
+            article['title'] = item.select_one('a.news_tit')['title']
+            article['summary'] = item.select_one(
+                'a.api_txt_lines.dsc_txt_wrap').text
+            article['press'] = item.select_one(
+                'div.news_info > div.info_group > a.info.press').text
+            article['link'] = item.select_one('a.news_tit')['href']
+
+            page_result.append(article)
+
+        return page_result
+
+    async def __scrap_main(self, max_scrapper=10):
+        """개별페이지 스크래핑 비동기 처리 함수 호출 및 결과 취합
+
+        Args:
+            max_scrapper (int, optional): async 처리 page scrapper 최대 수. Defaults to 10.
+
+        Returns:
+            list: news item dictionary의 list
+        """
+        header = {
+            'User-Agent': UserAgent(verify_ssl=False).chrome
+        }
+
+        main_result = []
+
+        while self.current < self.maximum:
+            start = self.current
+            end = self.maximum if self.current + \
+                max_scrapper > self.maximum else self.current + max_scrapper
+
+            print(f'\t[scrap_main] start: {start + 1} - end: {end}')
+            scrappers = []
+
+            for page in range(start + 1, end + 1):
+                scrappers.append(asyncio.create_task(
+                    self.__scrap_page(page, header)))
+
+            self.current = end
+
+            for page_result in await asyncio.gather(*scrappers):
+                # main_result 에 누적하고 있으나, memory 사용률을 고려하여 database 등 결과를 직접 저장 필요
+                main_result.extend(page_result)
+
+            print(f'\t[scrap_main] comulative results: {len(main_result)}')
+
+        return main_result
+
+    def gather(self):
+        """main_scrap 함수 호출자
+
+        Returns:
+            dictionary: 네이버 뉴스 스크래핑 dictionary의 list
+        """
+        start = time.perf_counter()
+        news = asyncio.run(self.__scrap_main())
+        print(
+            f'[newsscrapper-gather] elipsed time: {time.perf_counter() - start:0.2f} seconds')
+
+        return news
+
+    def info(self):
+        """환경 설정 확인용 함수
+        """
+        print(self.baseurl)
+        print(self.keyword)
+        print(self.maximum)
+
+
+if __name__ == '__main__':
+    scrapper = NewsScrapper('마이데이터', 3)
+    scrapper.gather()
